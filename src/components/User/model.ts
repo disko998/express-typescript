40bd846658d17155ccc811b7aeba732fbc7e21db
disko998 <disko998@gmail.com>
2020-07-22 12:40:38 +0200

    env config

--- src/components/User/model.ts
+++ src/components/User/model.ts
@@ -1,8 +1,8 @@
-import * as bcrypt from 'bcrypt';
-import * as connections from '../../config/connection/connection';
-import * as crypto from 'crypto';
-import { Document, Schema } from 'mongoose';
-import { NextFunction } from 'express';
+import * as bcrypt from 'bcrypt'
+import * as connections from '../../config/connection/connection'
+import * as crypto from 'crypto'
+import { Document, Schema } from 'mongoose'
+import { NextFunction } from 'express'
 
 /**
  * @export
@@ -10,29 +10,29 @@ import { NextFunction } from 'express';
  * @extends {Document}
  */
 export interface IUserModel extends Document {
-    email: string;
-    password: string;
-    passwordResetToken: string;
-    passwordResetExpires: Date;
+    email: string
+    password: string
+    passwordResetToken: string
+    passwordResetExpires: Date
 
-    facebook: string;
-    tokens: AuthToken[];
+    facebook: string
+    tokens: AuthToken[]
 
     profile: {
-        name: string,
-        gender: string,
-        location: string,
-        website: string,
+        name: string
+        gender: string
+        location: string
+        website: string
         picture: string
-    };
-    comparePassword: (password: string) => Promise < boolean > ;
-    gravatar: (size: number) => string;
+    }
+    comparePassword: (password: string) => Promise<boolean>
+    gravatar: (size: number) => string
 }
 
 export type AuthToken = {
-    accessToken: string,
+    accessToken: string
     kind: string
-};
+}
 
 /**
  * @swagger
@@ -63,64 +63,69 @@ export type AuthToken = {
  *      items:
  *        $ref: '#/components/schemas/UserSchema'
  */
-const UserSchema: Schema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        trim: true
+const UserSchema: Schema = new Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            trim: true,
+        },
+        password: String,
+        passwordResetToken: String,
+        passwordResetExpires: Date,
+        tokens: Array,
     },
-    password: String,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-    tokens: Array,
-}, {
-    collection: 'usermodel',
-    versionKey: false
-}).pre('save', async function (next: NextFunction): Promise < void > {
-    const user: any = this; // tslint:disable-line
+    {
+        collection: 'usermodel',
+        versionKey: false,
+    },
+).pre('save', async function (next: NextFunction): Promise<void> {
+    const user: any = this // tslint:disable-line
 
     if (!user.isModified('password')) {
-        return next();
+        return next()
     }
 
     try {
-        const salt: string = await bcrypt.genSalt(10);
+        const salt: string = await bcrypt.genSalt(10)
 
-        const hash: string = await bcrypt.hash(user.password, salt);
+        const hash: string = await bcrypt.hash(user.password, salt)
 
-        user.password = hash;
-        next();
+        user.password = hash
+        next()
     } catch (error) {
-        return next(error);
+        return next(error)
     }
-});
+})
 
 /**
  * Method for comparing passwords
  */
-UserSchema.methods.comparePassword = async function (candidatePassword: string): Promise < boolean > {
+UserSchema.methods.comparePassword = async function (
+    candidatePassword: string,
+): Promise<boolean> {
     try {
-        const match: boolean = await bcrypt.compare(candidatePassword, this.password);
+        const match: boolean = await bcrypt.compare(candidatePassword, this.password)
 
-        return match;
+        return match
     } catch (error) {
-        return error;
+        return error
     }
-};
+}
 
 /**
  * Helper method for getting user's gravatar.
  */
 UserSchema.methods.gravatar = function (size: number): string {
     if (!size) {
-        size = 200;
+        size = 200
     }
     if (!this.email) {
-        return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+        return `https://gravatar.com/avatar/?s=${size}&d=retro`
     }
-    const md5: string = crypto.createHash('md5').update(this.email).digest('hex');
+    const md5: string = crypto.createHash('md5').update(this.email).digest('hex')
 
-    return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
+    return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`
+}
 
-export default connections.db.model < IUserModel > ('UserModel', UserSchema);
+export default connections.db.model<IUserModel>('UserModel', UserSchema)
--- src/config/server/index.ts
+++ src/config/server/index.ts
@@ -1,18 +1,26 @@
-import * as http from 'http';
-import * as serverHandlers from './serverHandlers';
-import server from './server';
+import * as http from 'http'
+import * as colors from 'colors'
+import * as serverHandlers from './serverHandlers'
+import server from './server'
 
-const Server: http.Server = http.createServer(server);
+colors
+
+const Server: http.Server = http.createServer(server)
 
 /**
  * Binds and listens for connections on the specified host
  */
-Server.listen(server.get('port'));
+Server.listen(server.get('port'), () =>
+  console.log(
+    'Server listening on port:'.yellow.bold,
+    `${server.get('port')}`.blue.bold,
+  ),
+)
 
 /**
  * Server Events
  */
-Server.on('error',
-    (error: Error) => serverHandlers.onError(error, server.get('port')));
-Server.on('listening',
-    serverHandlers.onListening.bind(Server));
+Server.on('error', (error: Error) =>
+  serverHandlers.onError(error, server.get('port')),
+)
+Server.on('listening', serverHandlers.onListening.bind(Server))
